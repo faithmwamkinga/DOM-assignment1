8cd3263f2f681394b0593854cf958981d1686178
faithmwamkinga <faithmwamkinga@gmail.com>
2023-05-25 19:54:26 +0300

    first DOM assignment

---
+++ index.js
@@ -0,0 +1,9 @@
+document.body.style.backgroundColor=" #C0C0C0"
+document.getElementById("title").style.color="#51cf66"
+let caps=document.getElementsByTagName("h3").upper()
+let p=document.createElement("p")
+document.getElementById("frulist").appendChild(p)
+p.innerHTML="Pineapple"
+let x=document.createElement("x")
+document.getElementById("vegList").appendChild(x)
+x.innerHTML="Spinach"
